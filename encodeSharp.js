fdbd8945958a75da072aa22b9163f91251c5e673
Cher Stewart <cher.stewart@wwt.com>
2017-05-14 00:08:08 -0500

    const and arrow functions

--- encodeSharp.js
+++ encodeSharp.js
@@ -1,14 +1,14 @@
-var postcss = require('postcss');
-var encodeSharpPlugin = require('./lib/encodeSharpPostCSSPlugin');
+const postcss = require('postcss')
+const encodeSharpPlugin = require('./lib/encodeSharpPostCSSPlugin')
 
-module.exports = function(content, sourcemap) {
-  this.cacheable && this.cacheable();
-  var done = this.async();
+module.exports = (content, sourcemap) => {
+  this.cacheable && this.cacheable()
+  const done = this.async()
 
   postcss([ encodeSharpPlugin() ])
     .process(content)
-    .then(function (result) {
-      done(null, result.css, sourcemap);
+    .then( (result) => {
+      done(null, result.css, sourcemap)
     })
-    .catch(done);
-};
+    .catch(done)
+}
